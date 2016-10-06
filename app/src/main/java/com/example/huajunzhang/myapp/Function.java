1580beb65da30566f7e069e22a5bd6e5b28a3565
huajun zhang <zhj1990530@gmail.com>
2016-10-05 22:01:31 -0700

    comment

--- app/src/main/java/com/example/huajunzhang/myapp/Function.java
+++ app/src/main/java/com/example/huajunzhang/myapp/Function.java
@@ -16,22 +16,17 @@ import java.util.Locale;
 
 public class Function {
 
-	private static final String OPEN_WEATHER_MAP_URL =
-			"http://api.openweathermap.org/data/2.5/weather?lat=%s&lon=%s&units=metric";
+	//prepare openweather API url
 
     private static final String OPEN_WEATHER_MAP_URL_2 =
             "http://api.openweathermap.org/data/2.5/forecast/daily?lat=%s&lon=%s&units=metric&cnt=5";
-    //http://api.openweathermap.org/data/2.5/forecast/daily?units=metric&cnt=5
 
 	private static final String OPEN_WEATHER_MAP_API = "2cc497cafac898b5b9aae020a2040dc2";
 
 
-
-
-
+	//build Async Agruement
 	public interface AsyncResponse {
 
-		//void processFinish(String output1, String[] output2, String output3);
         void processFinish(String output1, String[] output2);
 
     }
@@ -48,10 +43,9 @@ public class Function {
 		protected JSONObject doInBackground(String... params) {
 
 			JSONObject jsonWeather = null;
-            //JSONObject jsonForecast = null;
 			try {
+				//generate json data
 				jsonWeather = getWeatherJSON(params[0], params[1]);
-                //jsonForecast = getForeastJSON(params[0], params[1]);
 
 
             } catch (Exception e) {
@@ -61,19 +55,15 @@ public class Function {
 
 			return jsonWeather;
 		}
+		 //json parse function
          @Override
          protected void onPostExecute(JSONObject json) {
              try {
                  if(json != null){
 
-					 //JSONObject details = json.getJSONArray("weather").getJSONObject(0);
                      JSONObject city = json.getJSONObject("city");
 
-                     //JSONObject list = json.getJSONArray("list");
-					 //System.err.println("json 1");
 					 DateFormat df = DateFormat.getDateInstance();
-					 //DateFormat df = DateFormat.getDateTimeInstance();
-					 //System.err.println("json 2");
 
 					 String cityName = city.getString("name").toUpperCase(Locale.US) + ", " + city.getString("country");
 
@@ -87,31 +77,29 @@ public class Function {
                          if (!tempForm()) {
                              temperatureMin = String.format("%.2f", toFahrenheit(temp.getDouble("min"))) + "°F";
                              temperatureMax = String.format("%.2f", toFahrenheit(temp.getDouble("max"))) + "°F";
-                             ListItems[i]= temperatureMin+"-"+temperatureMax;
 							 String updatedOn = df.format(new Date(day.getLong("dt")*1000));
+							 //trasnlate json data to string
+							 ListItems[i]= temperatureMin+"-"+temperatureMax;
 							 ListItems[i] += " " + updatedOn;
-                             //System.out.println(tempForm());
-                             //System.out.println(ListItems[i]);
+							 //merge string
 
-                         } else {
+
+
+						 } else {
                              temperatureMin = String.format("%.2f", temp.getDouble("min")) + "°C";
                              temperatureMax = String.format("%.2f", temp.getDouble("max")) + "°C";
-                             //System.out.println(tempForm());
-                             ListItems[i]= temperatureMin+"-"+temperatureMax;
 							 String updatedOn = df.format(new Date(day.getLong("dt")*1000));
+							 //trasnlate json data to string
+							 ListItems[i]= temperatureMin+"-"+temperatureMax;
 							 ListItems[i] += " " + updatedOn;
-							 //System.out.println(ListItems[i]);
+							 //merge string
 
-                         }
+
+						 }
                      }
 
-					 //String updatedOn = df.format(new Date(json.getLong("dt")*1000));
-
-
-
 					 delegate.processFinish(cityName, ListItems);
-					 //System.err.println("json 4");
-
+					 //send back result
 
 				 }else{
 					 System.err.println("json is null");
@@ -122,51 +110,19 @@ public class Function {
              }
          }
      }
-    /*
-		@Override
-		protected void onPostExecute(JSONObject json) {
-			try {
-			if(json != null){
-				JSONObject details = json.getJSONArray("weather").getJSONObject(0);
-				JSONObject main = json.getJSONObject("main");
-				DateFormat df = DateFormat.getDateTimeInstance();
-				String city = json.getString("name").toUpperCase(Locale.US) + ", " + json.getJSONObject("sys").getString("country");
-                String temperature="no data";
-                //call jni coversion C to F
-				if(!tempForm()){
-					 temperature = String.format("%.2f", toFahrenheit(main.getDouble("temp"))) + "°F";
-                     //System.out.println(tempForm());
-				}else{
-					 temperature = String.format("%.2f", main.getDouble("temp"))+ "°C";
-                     //System.out.println(tempForm());
 
-                }
-
-				String updatedOn = df.format(new Date(json.getLong("dt")*1000));
-
-
-				delegate.processFinish(city, temperature, updatedOn);
-
-			}
-			} catch (JSONException e) {
-                //Log.e(LOG_TAG, "Cannot process JSON results", e);
-            }
-		}
-	}
-*/
 
 	public static JSONObject getWeatherJSON(String lat, String lon){
 		try {
 			URL url = new URL(String.format(OPEN_WEATHER_MAP_URL_2, lat, lon));
-			//URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?lat=32.715736&lon=-117.161100&&units=metric&cnt=5");
 			HttpURLConnection connection =
 					(HttpURLConnection)url.openConnection();
-
+			//open connection
 			connection.addRequestProperty("x-api-key", OPEN_WEATHER_MAP_API);
-
+			//add api key for verify
 			BufferedReader reader = new BufferedReader(
 					new InputStreamReader(connection.getInputStream()));
-
+			//read json buffer bit by bit
 			StringBuffer json = new StringBuffer(1024);
 			String tmp="";
 			while((tmp=reader.readLine())!=null)
@@ -178,28 +134,24 @@ public class Function {
             // This value will be 404 if the request was not
 			// successful
 			if(data.getInt("cod") != 200){
-				//System.err.println("kkkkkkkkk");
 				return null;
 
 			}
-			//System.err.println("yyyyyyyyyy");
-
 			return data;
 		}catch(Exception e){
-			//System.err.println("jjjjjjjjjjjjjj");
 			return null;
 		}
 	}
 
 	public static native  double toFahrenheit(double n1);
-    //native class to conversion c->F:)
+    //native class to conversion C->F:)
 
 	public static native  boolean tempForm();
     //return current temperture format
 
 	static {
 		System.loadLibrary("native-lib");
-	}
+	}//connect with native library
 
 
 }
--- app/src/main/java/com/example/huajunzhang/myapp/MainActivity.java
+++ app/src/main/java/com/example/huajunzhang/myapp/MainActivity.java
@@ -1,58 +1,41 @@
 package com.example.huajunzhang.myapp;
 
-import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Html;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 import android.widget.CompoundButton;
-
-import android.app.Activity;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.os.Bundle;
-import android.widget.TextView;
-import android.widget.ArrayAdapter;
-import java.util.*;
-import android.view.View;
-import android.widget.AdapterView;
 
-import android.widget.Toast;
-import android.widget.AdapterView.OnItemClickListener;
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
 
-    //start new sensor manger
+    //create new sensor manger
     private SensorManager mgr;
     private Sensor temp;
-    private StringBuilder msg = new StringBuilder(2048);
 
-    static String[] values = new String[5];
-    TextView cityField, currentTemperatureField, updatedField, day1_view,day2_view, day3_view, day4_view, day5_view,description ;
-    //ListView listView ;
-    //Typeface weatherFont;
+    //create textviews
+    TextView cityField, currentTemperatureField, updatedField, day1_view,day2_view, day3_view,
+            day4_view, day5_view,description ;
 
+    //initiallize switcher temp format
     boolean switcher=true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        //create main active
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
+        //connect to sensor service
         mgr = (SensorManager) this.getSystemService(SENSOR_SERVICE);
-
+        //start sensor type = ambiant temperature
         temp = mgr.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
 
-        currentTemperatureField = (TextView)findViewById(R.id.current_temperature_field);
-        //currentTemperatureField.setIncludeFontPadding(false);
-        currentTemperatureField.setText("aaaa");
-        currentTemperatureField.setText("bbbb");
-        //weatherFont = Typeface.createFromAsset(getApplicationContext().getAssets(), "fonts/weathericons-regular-webfont.ttf");
+        //connect textview with xml id
         ToggleButton toggle = (ToggleButton) findViewById(R.id.toggleButton);
 
         toggle.setText("to Fahrenheit");
@@ -64,6 +47,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         toggle.setTextOn("to Celsius");
         // Sets the text for when the button is in the checked state.
 
+        currentTemperatureField = (TextView)findViewById(R.id.current_temperature_field);
         cityField = (TextView)findViewById(R.id.city_field);
         updatedField = (TextView)findViewById(R.id.updated_field);
         description =(TextView)findViewById(R.id.description);
@@ -72,28 +56,23 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         day3_view = (TextView)findViewById(R.id.day3);
         day4_view = (TextView)findViewById(R.id.day4);
         day5_view = (TextView)findViewById(R.id.day5);
-        //currentTemperatureField.setText("Sensor Data");
 
-        //detailsField = (TextView)findViewById(R.id.details_field);
-        //humidity_field = (TextView)findViewById(R.id.humidity_field);
-        //pressure_field = (TextView)findViewById(R.id.pressure_field);
-        //weatherIcon = (TextView)findViewById(R.id.weather_icon);
-        //weatherIcon.setTypeface(weatherFont);
+
         description.setText("Next 5 days temperature");
+        /*create toggle listener
+        * on click switch from c to F OR F  to c
+        * */
         toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
+
                     switcher=false;
+                    //call native methods to switch temp format
                     switchForm(switcher);
                     Function.placeIdTask asyncTask =new Function.placeIdTask(new Function.AsyncResponse() {
                         public void processFinish(String weather_city, String[] weather_temperature) {
-
-                            //public void processFinish(String weather_city, String weather_temperature,  String weather_updatedOn) {
-
+                            //display text
                             cityField.setText(weather_city);
-                            //updatedField.setText(weather_updatedOn);
-                            //values=weather_temperature;
-
                             day1_view.setText(weather_temperature[0]);
                             day2_view.setText(weather_temperature[1]);
                             day3_view.setText(weather_temperature[2]);
@@ -103,20 +82,16 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
                         }
                     });
-                    asyncTask.execute("32.715736", "-117.161100"); //  asyncTask.execute("Latitude", "Longitude")
+                    asyncTask.execute("32.715736", "-117.161100");
+                    //asyncTask.execute("Latitude", "Longitude")
                 } else {
                     switcher=true;
+                    //call native methods to switch temp format
                     switchForm(switcher);
                     Function.placeIdTask asyncTask =new Function.placeIdTask(new Function.AsyncResponse() {
                         public void processFinish(String weather_city, String[] weather_temperature) {
-                            //public void processFinish(String weather_city, String weather_temperature, String weather_updatedOn) {
-
+                            //display text
                             cityField.setText(weather_city);
-                            //updatedField.setText(weather_updatedOn);
-                            values=weather_temperature;
-                            //currentTemperatureField.setText(weather_temperature);
-                            //weatherIcon.setText(Html.fromHtml(weather_iconText));
-                            //System.out.println(values);
                             day1_view.setText(weather_temperature[0]);
                             day2_view.setText(weather_temperature[1]);
                             day3_view.setText(weather_temperature[2]);
@@ -133,84 +108,62 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         Function.placeIdTask asyncTask =new Function.placeIdTask(new Function.AsyncResponse() {
             public void processFinish(String weather_city, String[] weather_temperature) {
-                //public void processFinish(String weather_city, String weather_temperature, String weather_updatedOn) {
-
+                //display text
                 cityField.setText(weather_city);
-                //updatedField.setText(weather_updatedOn);
-                //detailsField.setText(weather_description);
                 day1_view.setText(weather_temperature[0]);
                 day2_view.setText(weather_temperature[1]);
                 day3_view.setText(weather_temperature[2]);
                 day4_view.setText(weather_temperature[3]);
                 day5_view.setText(weather_temperature[4]);
-                //values=weather_temperature;
-                //currentTemperatureField.setText(weather_temperature);
-
-                //System.out.println(values);
 
             }
         });
-        asyncTask.execute("32.715736", "-117.161100"); //  asyncTask.execute("Latitude", "Longitude")
+        asyncTask.execute("32.715736", "-117.161100");
+        //asyncTask.execute("Latitude", "Longitude")
 
-
-        /*listView = (ListView) findViewById(R.id.list);
-
-        // Assign adapter to ListView
-
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1,values);
-        //private ArrayAdapter<String> adapter = (ArrayAdapter<String>) getListAdapter();
-        //listView.setAdapter(adapter);
-        */
 
     }
 
+    //declare native methods switch temp format
     public static native  boolean switchForm(boolean CorF );
 
-    /* This is another native method declaration that is *not*
-     * implemented by 'hello-jni'. This is simply to show that
-     * you can declare as many native methods in your Java code
-     * as you want, their implementation is searched in the
-     * currently loaded native libraries only the first time
-     * you call them.
-     *
-     * Trying to call this function will result in a
-     * java.lang.UnsatisfiedLinkError exception !
-     */
-
+    //connect to native library
     static {
         System.loadLibrary("native-lib");
     }
 
+    /*overide abstract methods in sensor event listener
+    * */
     @Override
     protected void onResume() {
+        //resume sensor
         mgr.registerListener(this, temp, SensorManager.SENSOR_DELAY_NORMAL);
         super.onResume();
     }
 
     @Override
     protected void onPause() {
+        //stop sensor
         mgr.unregisterListener(this, temp);
         super.onPause();
     }
 
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
+            //nothing to do
     }
     @Override
     public void onSensorChanged(SensorEvent event) {
         String temperature;
+        //convert temperature to require format
         if (!Function.tempForm()) {
+            //call native methos from function convert c to f
             temperature = String.format("%.2f", Function.toFahrenheit(event.values[0])) + "°F";
 
         } else {
             temperature = String.format("%.2f", event.values[0]) + "°C";
 
         }
-        //msg.insert(0,  event.values[0]);
-        //currentTemperatureField.setText(msg);
-        //currentTemperatureField.invalidate();
+        //display text
         currentTemperatureField.setText(temperature);
 
     }
